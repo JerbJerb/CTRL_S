de47373e2d9415d6ee19426a5cf1cfe7476eb6f8
JerbJerb <jervin.galano01@gmail.com>
2021-06-27 04:00:30 +0800

    runggaaaaaaaaaaaaaaaa

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from object_detection import *
 from camera_settings import *
 
 
-application = Flask(__name__)
-Bootstrap(application)
+app = Flask(__name__)
+Bootstrap(app)
 
 
 check_settings()
@@ -20,13 +20,13 @@ VIDEO = VideoStreaming()
 cv2.destroyAllWindows()
 
 
-@application.route('/')
+@app.route('/')
 def home():
     TITLE = 'CTRL + S: Real Time Monitoring'
     return render_template('index.html', TITLE=TITLE)
 
 
-@application.route('/video_feed')
+@app.route('/video_feed')
 def video_feed():
     '''
     Video streaming route.
@@ -40,49 +40,49 @@ def video_feed():
 
 
 # Button requests called from ajax
-@application.route('/request_preview_switch')
+@app.route('/request_preview_switch')
 def request_preview_switch():
     VIDEO.preview = not VIDEO.preview
     print('*'*10, VIDEO.preview)
     return "nothing"
 
-@application.route('/request_flipH_switch')
+@app.route('/request_flipH_switch')
 def request_flipH_switch():
     VIDEO.flipH = not VIDEO.flipH
     print('*'*10, VIDEO.flipH)
     return "nothing"
 
-@application.route('/request_model_switch')
+@app.route('/request_model_switch')
 def request_model_switch():
     VIDEO.detect = not VIDEO.detect
     print('*'*10, VIDEO.detect)
     return "nothing"
 
-@application.route('/request_exposure_down')
+@app.route('/request_exposure_down')
 def request_exposure_down():
     VIDEO.exposure -= 1
     print('*'*10, VIDEO.exposure)
     return "nothing"
 
-@application.route('/request_exposure_up')
+@app.route('/request_exposure_up')
 def request_exposure_up():
     VIDEO.exposure += 1
     print('*'*10, VIDEO.exposure)
     return "nothing"
 
-@application.route('/request_contrast_down')
+@app.route('/request_contrast_down')
 def request_contrast_down():
     VIDEO.contrast -= 4
     print('*'*10, VIDEO.contrast)
     return "nothing"
 
-@application.route('/request_contrast_up')
+@app.route('/request_contrast_up')
 def request_contrast_up():
     VIDEO.contrast += 4
     print('*'*10, VIDEO.contrast)
     return "nothing"
 
-@application.route('/reset_camera')
+@app.route('/reset_camera')
 def reset_camera():
     STATUS = reset_settings()
     print('*'*10, STATUS)
@@ -90,4 +90,4 @@ def reset_camera():
 
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
